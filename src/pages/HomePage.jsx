7844150ea4ccf930a59a16b6156d90533d9ebbc3
idrisBellahsen <39699663+idrisBellahsen@users.noreply.github.com>
2020-01-16 17:03:49 +0100

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,5 +6,6 @@ export default () =>
     <Navbar/>
 <h1>Prochainement en librairie!</h1>
     <BookContainer/>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore consectetur illum a labore et, at repellat laboriosam reprehenderit. Dolor tempora nam cum pariatur earum, nostrum doloremque corporis ab at assumenda. Laboriosam provident similique aperiam excepturi quia tenetur id minus quos.</p>
   </div>
 ;
