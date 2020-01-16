bafe6cb6efb2256790fd390484dc3bac7c956437
idrisBellahsen <39699663+idrisBellahsen@users.noreply.github.com>
2020-01-16 17:07:23 +0100

    2

---
+++ src/pages/BookPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Book } from '../components';
+
+export default () =>
+<div>
+    <Navbar/>
+    <Book/>
+  </div>
+;
---
+++ src/pages/Works.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Book } from '../components';
+
+export default () =>
+<div>
+    <Navbar/>
+        Ma vie, mon oeuvre
+        <BookContainer/>
+
+  </div>
+;
